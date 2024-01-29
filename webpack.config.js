0cf36f44cbaf6e90fb0748e95d5315dcf925cb23
Shaun MacWilliam <shaun.macwilliam@gmail.com>
2024-01-29 23:57:05 +0000

    Environment set up
    Packages and dependencies installed. More config required

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-undef */
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    index: './index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Shaun MacWilliam Dev',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+    ],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+}
